6af99c62f72917fbe09ef1615a39c5a44b947de2
Scott Reynolds <scottreynolds6294@gmail.com>
2024-07-02 21:42:22 -0400

    fixed functionality issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // Retrieve tasks and nextId from localStorage
-let taskList = JSON.parse(localStorage.getItem("tasks"));
+let taskList = JSON.parse(localStorage.getItem("tasks")) || [];
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 
